4b1ce3c3660a28910b487eec6fd8ef3f441b2846
LeonarEQP <leonar.estupinanquinonez@plexus.es>
2024-06-11 18:48:58 +0200

    Ejercicio Contador incremento

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+  selector: 'app-counter',
+
+  template: `<h3>Counter: {{ counter }}</h3>
+
+  <button (click)="increseBy(+1)">+1</button>
+  <button (click)="resetBy()">Reset</button>
+  <button (click)="increseBy(-1)">-1</button>`,
+
+
+
+})
+
+export class CounterComponent {
+  public title: string = 'Hola Mundo';
+  public counter: number = 10;
+  increseBy(value: number): void {
+
+    this.counter += value;
+
+  }
+  resetBy(): void {
+
+    this.counter = 10;
+  }
+}
+
